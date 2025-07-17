bab7c53890cad3088075d16a27a23d32c9ccedee
osinoko <k023g0114@g.neec.ac.jp>
2025-07-17 10:20:55 +0900

    四角形のプログラム

--- DirectXGame/Resources/Model2/Model2.cpp
+++ DirectXGame/Resources/Model2/Model2.cpp
@@ -148,19 +148,19 @@ Model2* Model2::CreateSquare() {
 	// 左下
 	vertices[0].pos = {-1.0f, -1.0f, 0.0f};
 	vertices[0].uv = {0.0f, 1.0f};
-	vertices[0].normal = {0.0f, 0.0f, 1.0f};
+	vertices[0].normal = {0.0f, 0.0f, -1.0f};
 	// 左上
 	vertices[1].pos = {-1.0f, 1.0f, 0.0f};
 	vertices[1].uv = {0.0f, 0.0f};
-	vertices[1].normal = {0.0f, 0.0f, 1.0f};
+	vertices[1].normal = {0.0f, 0.0f, -1.0f};
 	// 右下
 	vertices[2].pos = {1.0f, -1.0f, 0.0f};
 	vertices[2].uv = {1.0f, 1.0f};
-	vertices[2].normal = {0.0f, 0.0f, 1.0f};
+	vertices[2].normal = {0.0f, 0.0f, -1.0f};
 	// 右上
 	vertices[3].pos = {1.0f, 1.0f, 0.0f};
 	vertices[3].uv = {1.0f, 0.0f};
-	vertices[3].normal = {0.0f, 0.0f, 1.0f};
+	vertices[3].normal = {0.0f, 0.0f, -1.0f};
 
 	// インデックス
 	indices[0] = 1; indices[1] = 3; indices[2] = 0;
